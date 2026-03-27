3ea0340da5e18b678d3cdaf786833af4d8743896
Ynna Gumban <27038728+ynna-m@users.noreply.github.com>
2026-03-27 11:01:35 -0500

    Updated Docking Logic. Removed references to System.Windows, and moved most of them to Avalonia

--- TabletFriend/TabletFriend/Docking/DockingMenuFactory.cs
+++ TabletFriend/TabletFriend/Docking/DockingMenuFactory.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
-using WpfAppBar;
+// using System.Windows.Controls;
+// using WpfAppBar;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace TabletFriend.Docking
 {
