07c2bfcdf67b1ad7e96496cce5ba4dc06c0b551f
Ynna Gumban <ygumban@myworkforcego.com>
2026-03-28 16:08:58 -0500

    Added App.axaml and  App.axaml.cs file

---
+++ TabletFriend/TabletFriend/App.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TabletFriend;
+
+public partial class App : Application
+{
+    public App()
+    {
+        
+    }
+}
